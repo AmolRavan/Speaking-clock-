e503e3ea52f3286625cd3c86a2d9b48e7dc94096
aravan <aravan@calibo.com>
2023-07-31 16:02:10 +0530

    swagger annotations

--- src/main/java/com/example/speakingclock/controller/SpeakingCloakController.java
+++ src/main/java/com/example/speakingclock/controller/SpeakingCloakController.java
@@ -2,6 +2,9 @@ package com.example.speakingclock.controller;
 
 import com.example.speakingclock.service.SpeakingClockService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,12 @@ public class SpeakingCloakController {
     @Autowired
     private SpeakingClockService speakingClockService;
 
+
+    @ApiOperation(value = "Gets Time in words", notes = "Time in words")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful response"),
+            @ApiResponse(code = 404, message = "Schema not found"),
+            @ApiResponse(code = 400, message = "Missing or invalid request/request body"),
+            @ApiResponse(code = 500, message = "Internal error")})
     @GetMapping("{timeInNumber}")
     public ResponseEntity<String> getConvertedTimingInWords(@PathVariable String timeInNumber){
         return new ResponseEntity<>(speakingClockService.getConvertedTimingInWords(timeInNumber), HttpStatus.OK);
